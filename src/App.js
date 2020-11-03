8118cd4eea1fb6b95e0d1a0acd00ccfe91559fdd
emranweb <emranbd77@gmail.com>
2020-11-03 16:32:05 +0600

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
 
 function App() {
   const [resource, setResource] = useState("posts");
+  const [data, setData] = useState([])
 
+  const getData = async (data)=>{
+    const response = await Axios.get(`https://jsonplaceholder.typicode.com/${data}`)
+    setData(response.data);
+  }
+
+  useEffect(() => {
+     getData(resource)
+  },[resource])
 
   return (
-     <div className="container">
-        <button type="submit" className="btn btn-primary" onClick={()=>setResource("posts")}>Post</button>
-        <button type="submit" className="btn btn-primary" onClick={()=>setResource("users")}>User</button>
-        <div>
-        {resource}
-        </div>
-     </div>  
+    <Container maxWidth="lg">
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        onClick={() => setResource("posts")}
+      >
+        Post
+      </Button>
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        onClick={() => setResource("users")}
+      >
+        User
+      </Button>
+      <div>{data.length}</div>
+    </Container>
   );
 }
 
